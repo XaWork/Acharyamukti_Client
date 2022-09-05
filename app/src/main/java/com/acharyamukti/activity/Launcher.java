17ecdc5422010abf1fb2901f60190d8a5f99dd7c
origin <imsangeetaangel@gmail.com>
2022-09-05 18:25:07 +0530

    bug fixing

--- app/src/main/java/com/acharyamukti/activity/Launcher.java
+++ app/src/main/java/com/acharyamukti/activity/Launcher.java
@@ -1,16 +1,19 @@
 package com.acharyamukti.activity;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.acharyamukti.R;
 import com.bumptech.glide.Glide;
 
 public class Launcher extends AppCompatActivity {
-    ImageView imageView;
+    ImageView imageView,profileImage;
 
 
     @Override
@@ -18,14 +21,27 @@ public class Launcher extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splace);
         imageView = findViewById(R.id.lunchLogo);
-    //    Glide.with(this).load(R.drawable.luncher_screen_new).into(imageView);
+        profileImage=findViewById(R.id.profileImage);
+        final ValueAnimator anim = ValueAnimator.ofFloat(1f, 1.5f);
+        anim.setDuration(1000);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                profileImage.setScaleX((Float) animation.getAnimatedValue());
+                profileImage.setScaleY((Float) animation.getAnimatedValue());
+            }
+        });
+        anim.setRepeatCount(1);
+        anim.setRepeatMode(ValueAnimator.REVERSE);
+        anim.start();
+    //    Glide.with(this).load(R.drawable.bg_image2).into(imageView);
         new Handler().postDelayed(() -> {
-            SharedPreferences sharedPreferences=getSharedPreferences(Login.PRES_NAME,MODE_PRIVATE);
-            boolean hasLoggedIn=sharedPreferences.getBoolean("hasLoggedIn",false);
+            SharedPreferences sharedPreferences = getSharedPreferences(Login.PRES_NAME, MODE_PRIVATE);
+            boolean hasLoggedIn = sharedPreferences.getBoolean("hasLoggedIn", false);
             Intent intent;
-            if (hasLoggedIn){
+            if (hasLoggedIn) {
                 intent = new Intent(getApplicationContext(), DashBoard.class);
-            }else {
+            } else {
                 intent = new Intent(getApplicationContext(), Login.class);
             }
             startActivity(intent);
