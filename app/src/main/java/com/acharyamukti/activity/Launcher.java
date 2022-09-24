8e6caa9d8ec4c1750b3a9681dcf521022e90e68f
origin <imsangeetaangel@gmail.com>
2022-09-24 18:16:31 +0530

    some UI changes and bug fixing

--- app/src/main/java/com/acharyamukti/activity/Launcher.java
+++ app/src/main/java/com/acharyamukti/activity/Launcher.java
@@ -1,13 +1,17 @@
 package com.acharyamukti.activity;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.acharyamukti.R;
 import com.acharyamukti.helper.Backend;
+import com.bumptech.glide.Glide;
 
 public class Launcher extends AppCompatActivity {
     ImageView imageView, profileImage;
@@ -17,28 +21,28 @@ public class Launcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splace);
- //       imageView = findViewById(R.id.lunchLogo);
-//        profileImage = findViewById(R.id.profileImage);
-//        final ValueAnimator anim = ValueAnimator.ofFloat(1f, 1.5f);
-//        anim.setDuration(1000);
-//        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator animation) {
-//                profileImage.setScaleX((Float) animation.getAnimatedValue());
-//                profileImage.setScaleY((Float) animation.getAnimatedValue());
-//            }
-//        });
-//        anim.setRepeatCount(1);
-//        anim.setRepeatMode(ValueAnimator.REVERSE);
-//        anim.start();
-        //    Glide.with(this).load(R.drawable.bg_image2).into(imageView);
+        imageView = findViewById(R.id.lunchLogo);
+        profileImage = findViewById(R.id.profileImage);
+        final ValueAnimator anim = ValueAnimator.ofFloat(1f, 1.5f);
+        anim.setDuration(1000);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                profileImage.setScaleX((Float) animation.getAnimatedValue());
+                profileImage.setScaleY((Float) animation.getAnimatedValue());
+            }
+        });
+        anim.setRepeatCount(1);
+        anim.setRepeatMode(ValueAnimator.REVERSE);
+        anim.start();
+       // Glide.with(this).load(R.drawable.bg_image2).into(imageView);
         String userid = Backend.getInstance(this).getUserId();
 
         new Handler().postDelayed(() -> {
             SharedPreferences sharedPreferences = getSharedPreferences(Login.PRES_NAME, MODE_PRIVATE);
             boolean hasLoggedIn = sharedPreferences.getBoolean("hasLoggedIn", false);
             Intent intent;
-            if (hasLoggedIn && userid.length()!= 0) {
+            if (hasLoggedIn && userid.length() != 0) {
                 intent = new Intent(getApplicationContext(), DashBoard.class);
             } else {
                 intent = new Intent(getApplicationContext(), Login.class);
