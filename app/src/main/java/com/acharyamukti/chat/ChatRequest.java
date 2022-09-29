b5125ea0026ec4e07df7b175e28cc4bae61a20d6
origin <imsangeetaangel@gmail.com>
2022-09-29 18:29:07 +0530

    UI design for chatRequest

---
+++ app/src/main/java/com/acharyamukti/chat/ChatRequest.java
@@ -0,0 +1,16 @@
+package com.acharyamukti.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.acharyamukti.R;
+
+public class ChatRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_request);
+    }
+}
